a058f27a8813d8746490cb9d113077d316cb7189
George Moroz <agricolamz@gmail.com>
2023-04-11 20:08:13 +0300

    update

---
+++ visualizing_scripts.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(lubridate)
+df <- readxl::read_xlsx("data.xlsx")
+
+df %>% 
+  mutate(date = lubridate::make_date(year = year, month = month, day = day)) %>% 
+  filter(year > 2018, year < 2023) %>% 
+  ggplot(aes(date))+
+  geom_dotplot()+
+  facet_wrap(~year, scales = "free")+
+  theme_minimal()
+  
+df %>% 
+  mutate(author = str_remove_all(author, "\\(.*?\\)"),
+         author = str_split(author, ", ")) %>% 
+  unnest_longer(author) %>% 
+  mutate(author = str_squish(author)) %>% 
+  count(author, sort = TRUE) %>% 
+  filter(n > 1) %>%
+  mutate(author = fct_reorder(author, n)) %>% 
+  ggplot(aes(n, author))+
+  geom_col()+
+  labs(x = "", y = "")+
+  theme_minimal()
+  
